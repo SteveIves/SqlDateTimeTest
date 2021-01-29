9c8b5fc59d843cf58fdea18c7c8e47ff6f3bfa73
Steve Ives <stevei@synergex.com>
2021-01-29 11:57:43 -0800

    Improved the example so that the Synergy record has standard Synergy date (D8/D6) and time (D6 and D4) fields.

--- SqlDateTimeTest/CreateMySqlDatabase.sql
+++ SqlDateTimeTest/CreateMySqlDatabase.sql
@@ -6,9 +6,23 @@ use synergytestdb;
 
 CREATE TABLE Employee (
 EmpId DECIMAL(6) NOT NULL,
+EmpFirstName VARCHAR(20) NOT NULL,
+EmpLastName  VARCHAR(20) NOT NULL,
+EmpDept VARCHAR(15),
 EmpHireDate DATE,
 EmpHireTime TIME,
-EmpDateOfBirth DATETIME
+EmpPhoneWork  VARCHAR(14),
+EmpPhoneCell  VARCHAR(14),
+EmpPaid DECIMAL(1),
+EmpDateOfBirth DATETIME,
+EmpEmail  VARCHAR(40),
+EmpAddressStreet  VARCHAR(30),
+EmpAddressCity  VARCHAR(20),
+EmpAddressState  VARCHAR(2),
+EmpAddressZip  DECIMAL(5)
 );
 
-insert into Employee(EmpId,EmpHireDate,EmpHireTime,EmpDateOfBirth) values (1,"1997-11-01","09:00:00","1964-07-19 03:00:00");
+insert into Employee(EmpId,EmpFirstName,EmpLastName,EmpDept,EmpHireDate,EmpHireTime,EmpPhoneWork,EmpPhoneCell,EmpPaid,EmpDateOfBirth,EmpEmail,EmpAddressStreet,EmpAddressCity,EmpAddressState,EmpAddressZip) 
+ values (1,"Jane","Doe","PSG","1997-12-01","10:00:00","(555) 765-4321","(555) 765-4322",0,"1964-08-02 19:43:00","jane.doe@gmail.com","123 Main Street","Springfield","MA",12345);
+insert into Employee(EmpId,EmpFirstName,EmpLastName,EmpDept,EmpHireDate,EmpHireTime,EmpPhoneWork,EmpPhoneCell,EmpPaid,EmpDateOfBirth,EmpEmail,EmpAddressStreet,EmpAddressCity,EmpAddressState,EmpAddressZip)
+ values (2,"John","Doe","PSG","1997-11-01","09:00:00","(555) 123-4567","(555) 123-4568",1,"1964-07-19 03:00:00","john.doe@gmail.com","123 Main Street","Springfield","MA",12345);
